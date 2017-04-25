bd83cf9d743cfabf246492bf28f6d53d34301b05
Tanner <Tanner-Wang@users.noreply.github.com>
2017-04-25 22:07:16 +0800

    fix bug

--- app/src/main/java/com/example/android/baseballgamecounter/MainActivity.java
+++ app/src/main/java/com/example/android/baseballgamecounter/MainActivity.java
@@ -7,13 +7,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int sumScoreForA;
-    int sumScoreForB;
+    private int sumScoreForA;
+    private int sumScoreForB;
+    private final int SIX_POINTS = 6;
+    private final int THREE_POINTS = 3;
+    private final int TWO_POINTS = 2;
+    private final int ONE_POINT = 1;
+    ViewHolder holder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        holder.teamAScore = (TextView) findViewById(R.id.scoreForTeamA);
+        holder.teamBScore = (TextView) findViewById(R.id.scoreForTeamB);
     }
 
     public void resetTheScore(View view) {
@@ -24,52 +31,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Add6PointsForTeamA(View view) {
-        sumScoreForA += 6;
+        sumScoreForA += SIX_POINTS;
         displayForA(sumScoreForA);
     }
 
     public void Add3PointsForTeamA(View view) {
-        sumScoreForA += 3;
+        sumScoreForA += THREE_POINTS;
         displayForA(sumScoreForA);
     }
 
     public void Add2PointsForTeamA(View view) {
-        sumScoreForA += 2;
+        sumScoreForA += TWO_POINTS;
         displayForA(sumScoreForA);
     }
 
     public void Add1PointsForTeamA(View view) {
-        sumScoreForA += 1;
+        sumScoreForA += ONE_POINT;
         displayForA(sumScoreForA);
     }
 
     public void Add6PointsForTeamB(View view) {
-        sumScoreForB += 6;
+        sumScoreForB += SIX_POINTS;
         displayForB(sumScoreForB);
     }
 
     public void Add3PointsForTeamB(View view) {
-        sumScoreForB += 3;
+        sumScoreForB += THREE_POINTS;
         displayForB(sumScoreForB);
     }
 
     public void Add2PointsForTeamB(View view) {
-        sumScoreForB += 2;
+        sumScoreForB += TWO_POINTS;
         displayForB(sumScoreForB);
     }
 
     public void Add1PointsForTeamB(View view) {
-        sumScoreForB += 1;
+        sumScoreForB += ONE_POINT;
         displayForB(sumScoreForB);
     }
 
     private void displayForA(int scoreOfTeamA) {
-        TextView t = (TextView) findViewById(R.id.scoreForTeamA);
-        t.setText(String.valueOf(scoreOfTeamA));
+        holder.teamAScore.setText(String.valueOf(scoreOfTeamA));
     }
 
     private void displayForB(int scoreOfTeamB) {
-        TextView t = (TextView) findViewById(R.id.scoreForTeamB);
-        t.setText(String.valueOf(scoreOfTeamB));
+        holder.teamBScore.setText(String.valueOf(scoreOfTeamB));
+    }
+
+    private class ViewHolder{
+        TextView teamAScore;
+        TextView teamBScore;
     }
 }
